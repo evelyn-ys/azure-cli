95188f28c24faa8e73869b934cd4a55107597b9b
evelyn-ys <yishiwang@microsoft.com>
2022-12-05 14:45:52 +0800

    test download artifacts

---
+++ scripts/regression_test/debug_script.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+import logging
+import os
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+
+instance_cnt = int(sys.argv[1])
+instance_idx = int(sys.argv[2])
+working_directory = "/mnt/vss/_work/1/s"
+
+
+def main():
+    build_id = os.getenv('BUILD_BUILDID')
+    print(f"The build id is {build_id}")
+
+    test_result_fp = os.path.join(working_directory, 'test_results.txt')
+    with open(test_result_fp, 'a') as f:
+        f.write(f"results {instance_idx}/{instance_cnt}:\n")
+        f.write("success 100%")
+
+
+if __name__ == '__main__':
+    main()
