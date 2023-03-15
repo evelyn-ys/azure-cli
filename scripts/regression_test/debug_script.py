25bd0dafe42ecb6eddae25d1db7275b1d9fa43fb
evelyn-ys <yishiwang@microsoft.com>
2023-03-15 16:47:47 +0800

    test extension

--- scripts/regression_test/debug_script.py
+++ scripts/regression_test/debug_script.py
@@ -9,6 +9,7 @@
 import logging
 import os
 import sys
+import subprocess
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -21,14 +22,47 @@ instance_idx = int(sys.argv[2])
 working_directory = "~/.azdev/env_config/mnt/vss/_work/1/s/env"
 
 
-def main():
-    build_id = os.getenv('BUILD_BUILDID')
-    print(f"The build id is {build_id}")
+def run_command(cmd, check_return_code=False):
+    error_flag = False
+    logger.info(cmd)
+    try:
+        out = subprocess.run(cmd, check=True)
+        if check_return_code and out.returncode:
+            raise RuntimeError(f"{cmd} failed")
+    except subprocess.CalledProcessError:
+        error_flag = True
+    return error_flag
 
-    test_result_fp = os.path.join(working_directory, f'test_results_{instance_idx}.txt')
-    with open(os.path.expanduser(test_result_fp), 'a') as f:
-        f.write(f"results {instance_idx}/{instance_cnt}:\n")
-        f.write("success 100%")
+
+def install_extension(extension_module):
+    try:
+        cmd = ['azdev', 'extension', 'add', extension_module]
+        error_flag = run_command(cmd, check_return_code=True)
+    except Exception:
+        error_flag = True
+
+    return error_flag
+
+
+def remove_extension(extension_module):
+    try:
+        cmd = ['azdev', 'extension', 'remove', extension_module]
+        error_flag = run_command(cmd, check_return_code=True)
+    except Exception:
+        error_flag = True
+
+    return error_flag
+
+
+def main():
+    module = "functionapp"
+    error_flag = install_extension(module)
+    logger.info(f"Finish installing extension, error_flag:{error_flag}")
+    if not error_flag:
+        cmd = ['azdev', 'test', module, '--discover', '--no-exitfirst', '--verbose', '--pytest-args', '"--durations=10"']
+        error_flag = run_command(cmd)
+        logger.info(f"Finish testing extension, error_flag:{error_flag}")
+    remove_extension(module)
 
 
 if __name__ == '__main__':
