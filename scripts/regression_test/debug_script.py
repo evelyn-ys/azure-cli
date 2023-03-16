c43c065b0ddad80da93dec8d76f81dbf6df0dc43
evelyn-ys <yishiwang@microsoft.com>
2023-03-16 16:00:03 +0800

    snake case

--- scripts/regression_test/debug_script.py
+++ scripts/regression_test/debug_script.py
@@ -69,8 +69,8 @@ def remove_extension(extension_module):
 
 def main():
     print(f"working directory: {os.getenv('BUILD_SOURCESDIRECTORY')}")
-    print(f"cli repo path: {os.getenv('CLI_REPO', None)}")
-    print(f"extension repo path: {os.getenv('CLI_EXTENSION_REPO', None)}")
+    print(f"cli repo path: {os.getenv('CLIREPO', None)}")
+    print(f"extension repo path: {os.getenv('EXTENSIONREPO', None)}")
     # modules = ["functionapp", "alertsmanagement"]
     # for module in modules:
     #     error_flag = install_extension(module)
